11b6772fb85684d9aeb887928feec7ae013d7fd2
OfirTeneJuno <ofir@junojourney.com>
2025-05-10 14:32:56 +0300

    d

--- src/components/common/TableContent.tsx
+++ src/components/common/TableContent.tsx
@@ -11,38 +11,36 @@ interface TableContentProps<T> {
  * TableContent component - renders table rows based on provided data
  */
 interface TableContentProps<T extends { id: string | number }> {
-    columns: Column<T>[];
-    data: T[];
+  columns: Column<T>[];
+  data: T[];
 }
 
-const TableContent = <T extends { id: string | number }>({ columns, data }: TableContentProps<T>) => {
-    console.log("TableContent data", data);
-
-    if (data.length === 0) {
-        return (
-            <TableRow>
-                <TableCell colSpan={columns.length} align="center">
-                    No data available
-                </TableCell>
-            </TableRow>
-        );
-    }
+const TableContent = <T extends { id: string | number }>({
+  columns,
+  data,
+}: TableContentProps<T>) => {
+  if (data.length === 0) {
     return (
-        <>
-            {data.map((rowData) => (
-                <TableRow
-                    key={rowData.id}
-                    sx={{ "&:hover": { bgcolor: "#f5f5f5" } }}
-                >
-                    {columns.map((column) => (
-                        <TableCell key={column.id} align="center">
-                            {column.render(rowData)}
-                        </TableCell>
-                    ))}
-                </TableRow>
-            ))}
-        </>
+      <TableRow>
+        <TableCell colSpan={columns.length} align="center">
+          No data available
+        </TableCell>
+      </TableRow>
     );
+  }
+  return (
+    <>
+      {data.map((rowData) => (
+        <TableRow key={rowData.id} sx={{ "&:hover": { bgcolor: "#f5f5f5" } }}>
+          {columns.map((column) => (
+            <TableCell key={column.id} align="center">
+              {column.render(rowData)}
+            </TableCell>
+          ))}
+        </TableRow>
+      ))}
+    </>
+  );
 };
 
 export default TableContent;
