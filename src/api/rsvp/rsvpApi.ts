d4029f0f32b3250c2149cef2278881b0cc1f9ae7
OfirTeneJuno <ofir@junojourney.com>
2025-07-21 22:06:16 +0300

    fix

--- src/api/rsvp/rsvpApi.ts
+++ src/api/rsvp/rsvpApi.ts
@@ -52,15 +52,21 @@ export interface SentMessage {
 }
 
 const getBaseUrl = (): string => {
-  const isDevelopment =
-    process.env.NODE_ENV === "development" ||
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isLocal =
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1";
-  if (isDevelopment) {
+  const isProduction = process.env.NODE_ENV === "production";
+  if (isLocal) {
     return "http://127.0.0.1:5001/wedding-c89a1/us-central1/app";
-  } else {
+  }
+  if (isProduction) {
+    return "https://app-ga5vulihbq-uc.a.run.app";
+  }
+  if (isDevelopment) {
     return "https://app-fhntq3wlyq-uc.a.run.app";
   }
+  return "http://127.0.0.1:5001/wedding-c89a1/us-central1/app";
 };
 
 const BASE_URL = getBaseUrl();
